15b8b785a5cb22f98fd5ad35bdb7e078f649eaf5
Rionaldo16 <siamanrionaldo16@gmail.com>
2024-05-16 01:29:05 +0800

    exercise

---
+++ db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  password: "123456",
+  host: "localhost",
+  port: 5432, // default Postgres port
+  database: "backEnd",
+});
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
